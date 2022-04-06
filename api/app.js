ac83ef2512e11e148abe7080897416614853925b
Andre Santos <andredupdm@gmail.com>
2022-04-06 19:21:32 -0300

    change app script

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Your WebServer');
+  response.send('Your NodeServer');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
